12b2dc7c790003ea4f9bfea5861cf017c4f2bae3
Juan Pablo Mendoza Amaya <juanpamendoza_08@hotmail.com>
2021-09-22 18:18:22 -0500

    Envio de seguridad por si se va la luz

--- Presentación/Program.cs
+++ Presentación/Program.cs
@@ -10,6 +10,37 @@ namespace Presentación
     {
         static void Main(string[] args)
         {
+            string identificacion, nombre, sexo;
+            int edad;
+            decimal pulsacion;
+
+            Console.WriteLine("Digite la Identificacion: ");
+            identificacion = Console.ReadLine();
+
+            Console.WriteLine("Digite El Nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Digite El sexi (M/F): ");
+            sexo = Console.ReadLine();
+           
+            Console.WriteLine("Digite la Edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            if (sexo.ToUpper().Equals("M"))
+            {
+                pulsacion = (210 - edad) / 10;
+
+            }else if (sexo.ToUpper().Equals("F"))
+            {
+                pulsacion = (220 - edad) / 10;
+            }
+            else
+            {
+                pulsacion = 0;
+            }
+            Console.WriteLine($"Su pulsacion es {pulsacion}");
+            Console.ReadKey();
+
         }
     }
 }
