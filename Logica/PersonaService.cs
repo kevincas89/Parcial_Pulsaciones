4b55a102fde76080b4e3f8d126b2c605a8c6ce1d
Juan Pablo Mendoza Amaya <juanpamendoza_08@hotmail.com>
2021-09-24 20:44:29 -0500

    Toda mi parte hecha, pero aun faltan un par de cosas

--- Logica/PersonaService.cs
+++ Logica/PersonaService.cs
@@ -21,13 +21,69 @@ namespace Logica
         {
             try
             {
-                registroPersona.Guardar(persona);
-                return "Datos guardados correctamente";
+                if (registroPersona.BuscarPorIdentificacion(persona.Identificacion)==null)
+                {
+                    registroPersona.Guardar(persona);
+                    return "Datos guardados correctamente";
+                }
+                return $"La persona con la identificacion {persona.Identificacion} ya se encuentra registrada.";
+                
             }
             catch (Exception exception)
             {
 
                 return "Ocurrieron los siguientes errores: " + exception.Message;
+            }
+        }
+
+        public string Eliminar(string identificacion)
+        {
+            try
+            {
+                if (registroPersona.BuscarPorIdentificacion(identificacion)!=null)
+                {
+                    registroPersona.Eliminar(identificacion);
+                    return $"Se elimino a la persona con la identificacion {identificacion}";
+                }
+                return $"No se encontro a la persona con identificacion{identificacion}";
+            }
+            catch (Exception exception)
+            {
+
+                return "Ocurrieron los siguientes errores: " + exception.Message;
+            }
+        }
+
+        public string Modificar(Persona personaNueva, string identificacion)
+        {
+            try
+            {
+                if (registroPersona.BuscarPorIdentificacion(identificacion) != null)
+                {
+                    registroPersona.Modificar(personaNueva,identificacion);
+                    return $"Se modifico a la persona con la identificacion {identificacion}";
+                }
+                return $"No se encontro a la persona con identificacion{identificacion}";
+            }
+            catch (Exception exception)
+            {
+
+                return "Ocurrieron los siguientes errores: " + exception.Message;
+            }
+        }
+
+        public BusquedaReponse Buscar(string identificacion)
+        {
+            try
+            {
+                ///Retorna una persona
+                return new BusquedaReponse(registroPersona.BuscarPorIdentificacion(identificacion));
+                
+            }
+            catch (Exception exception)
+            {
+                ///Retorna un string 
+                return new BusquedaReponse("Ocurrieron los siguientes errores: " + exception.Message);
             }
         }
 
@@ -60,6 +116,26 @@ namespace Logica
         }
 
         public ConsultaResponse(String mensaje)
+        {
+            Mensaje = mensaje;
+            Error = true;
+        }
+    }
+
+    public class BusquedaReponse
+    {
+        public Persona persona { get; set; }
+
+        public string Mensaje { get; set; }
+        public bool Error { get; set; }
+
+        public BusquedaReponse(Persona personas)
+        {
+            personas = personas;
+            Error = false;
+        }
+
+        public BusquedaReponse(String mensaje)
         {
             Mensaje = mensaje;
             Error = true;
